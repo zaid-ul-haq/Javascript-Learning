c8f6fd20906711d7a56efd065a74d60605160271
Zaid <zaid.ul.haq.2005@gmail.com>
2025-12-31 12:40:56 +0500

    Day-11: Difference Between Expression and Statement

---
+++ 01-Fundamentals-Part-1/15-Statement-and-Expression/script.js
@@ -0,0 +1,17 @@
+//=========Expression=========//
+// Defination: Expression is a piece of code that produces a value
+3 + 4
+2005
+true && true && !false
+// These are all Expressions
+
+//========Statement===========//
+// Defination: A statement is a complete instruction that tells the computer to perform a specific action.
+if (23 > 20) {
+    const str = "23 is bigger than 20.";
+}
+// This whole if else code is a Statement
+
+// In templete literals we can only insert expression, can't write statement
+console.log(`I'm ${2025 - 2002} years old.`);
+// ${We can add expression only here, not statement}
