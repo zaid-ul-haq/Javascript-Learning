fc682095d0b82277f2c0e7914dc6dc4792785c92
Zaid <zaid.ul.haq.2005@gmail.com>
2026-01-04 23:22:25 +0500

    Day-12: Function Review

---
+++ 02-Fundamentals-Part-2/22-Reviewing-Function/script.js
@@ -0,0 +1,22 @@
+
+
+const calcAge = function (birthYear) {
+    return 2026 - birthYear;
+}
+
+const yearLeftForRetirement = function (birthYear, name) {
+    const age = calcAge(birthYear);
+
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${name} retires in ${retirement} Years.`);
+        return retirement; //Always return in numbers | Good Practice
+    } else {
+        console.log(`${name} is already retired.`);
+        return -1; //Always return in numbers | Good Practice
+    }
+}
+
+console.log(yearLeftForRetirement(2002, "Zaid"));
+console.log(yearLeftForRetirement(1950, "Jhon"));
