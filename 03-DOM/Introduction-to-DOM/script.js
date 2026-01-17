2bdea8bb37c91b368cdc15c156407b3dc9dba0cf
Zaid <zaid.ul.haq.2005@gmail.com>
2026-01-17 20:24:00 +0500

    Day-24 + 25 : Introduction to DOM | Handling Click Event

--- 03-DOM/Introduction-to-DOM/script.js
+++ 03-DOM/Introduction-to-DOM/script.js
@@ -11,6 +11,7 @@ document.querySelector(".game__input").value = 14;
  */
 
 const secretNumber = 14; // The Correct Secret number
+let score = 20;
 // Handling Event Click
 // When We click on a button, the function will called..
 document.querySelector(".btn--primary").addEventListener("click", function () {
@@ -18,13 +19,30 @@ document.querySelector(".btn--primary").addEventListener("click", function () {
   // console.log(guess);
 
   // Will Check Condition, according to that the Status Test will change
-  if (!guess) {
+  if (guess <= 0) {
     document.querySelector(".game__message").textContent =
       "⛔No Number Entered!";
   } else if (guess === secretNumber) {
     document.querySelector(".game__message").textContent = "🎉 Correct Number";
-  } else {
-    document.querySelector(".game__message").textContent =
-      "❌ Incorrect! Try different Number";
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector(".game__message").textContent =
+        "📈The number is High!";
+      score--;
+      document.querySelector(".game_score").textContent = score;
+    } else {
+      document.querySelector(".game__message").textContent =
+        "💥You lost the game!";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector(".game__message").textContent =
+        "📉The number is Low!";
+      score--;
+      document.querySelector(".game_score").textContent = score;
+    } else {
+      document.querySelector(".game__message").textContent =
+        "💥You lost the game!";
+    }
   }
 });
